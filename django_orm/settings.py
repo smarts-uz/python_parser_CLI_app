5670bb229843d76f128b90142aac4b6daaf955b9
HunterZwer <117033000+HunterZwer@users.noreply.github.com>
2023-11-24 20:00:11 +0500

    Adding new sentry to settings

--- django_orm/settings.py
+++ django_orm/settings.py
@@ -1,4 +1,5 @@
 import os
+import sentry_sdk
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -9,14 +10,25 @@ SECRET_KEY = "6few3nci_q_o@l1dlbk81%wcxe!*6r29yu629&d97!hiqat9fa"
 DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'PASSWORD': 'MkTbWEfE7YBhjj8l',
+#         'HOST': 'db.jzqvdexjreyryogyhmdr.supabase.co',
+#         'PORT': 5432,
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'MkTbWEfE7YBhjj8l',
-        'HOST': 'db.jzqvdexjreyryogyhmdr.supabase.co',
-        'PORT': 5432,
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -38,6 +50,18 @@ DATABASES = {
 }
 """
 
+
+
+sentry_sdk.init(
+    dsn="https://a5a81e13a7bd554d379556da6e0bdc0f@o4506200236687360.ingest.sentry.io/4506281433825280",
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=1.0,
+)
 
 INSTALLED_APPS = ("django_orm.db",)
 # INSTALLED_APPS = ("db",)
