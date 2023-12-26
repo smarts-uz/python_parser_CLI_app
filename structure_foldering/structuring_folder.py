45465919f974320ab634dae629e46e5f88477478
HunterZwer <117033000+HunterZwer@users.noreply.github.com>
2023-12-26 19:36:08 +0500

    addinf rich and 3log to foldering

--- structure_foldering/structuring_folder.py
+++ structure_foldering/structuring_folder.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 import shutil
 from .functions import correct_data_title, correct_video_title, correct_post_title, correct_file_location, correct_url_name, create_link, create_video_note, create_folder_to_save
 from rich import print
+from log3 import Logger
+statistic = Logger('statictics', 'a')
 
 
 def create_dirs_all(list_of_data, main_folder_name,name_list):
@@ -48,13 +50,18 @@ def create_dirs_all(list_of_data, main_folder_name,name_list):
                             shutil.copy(file_location, actual_path_dir)
                             with open(f'{actual_path_dir}{from_name}.tmnote', 'x', encoding='UTF-8') as file:
                                 file.write(f'''From_name: {from_name}\n{video_title}''')
-                            print(actual_path + content + '___Succes_2!')
+                            msg = "[wheat1]"+actual_path + content + '___Succes_2!'
+                            statistic.log(msg)
+                            print(msg)
+
                         except Exception as e:
                             try:
                                 with open(f'{actual_path_dir}{from_name}.tmnote', 'a', encoding='UTF-8') as file:
                                     file.write(f'''\n{video_title}''')
                             except:
-                                print(f'[red] {content} - unable to create tmnote')
+                                msg = f'[wheat1] {content} - unable to create tmnote'
+                                statistic.log(msg)
+                                print(msg)
                             pass
                 elif content.startswith('video'):
                     # actual_path = path_to_save
@@ -70,9 +77,13 @@ def create_dirs_all(list_of_data, main_folder_name,name_list):
                             file_location = base_dir + '\\' + file_path
                             shutil.copy(file_location, actual_path_dir + video_title)
                             create_video_note(actual_path_dir, video_title, description, video_duration, from_name)
-                            print(actual_path_dir + video_title + '___Succes_1!')
+                            msg = "[purple]"+actual_path_dir + video_title + '___Succes_1!'
+                            statistic.log(msg)
+                            print(msg)
                         except:
-                            print(f'[red] {content} - unable to create folder')
+                            msg = f'[red] {content} - unable to create folder'
+                            statistic.err(msg)
+                            print(msg)
                             pass
                 elif content.startswith('https'):
                     for i in channel_text:
@@ -85,7 +96,9 @@ def create_dirs_all(list_of_data, main_folder_name,name_list):
                         try:
                             create_link(actual_path_dir, correct_url_name(content), content)
                         except:
-                            print(f'[red] {content} - unable to create link')
+                            msg = f'[red] {content} - unable to create link'
+                            statistic.err(msg)
+                            print(msg)
                             pass
             else:
                 if content.startswith('files') or content.startswith('photos'):
@@ -103,13 +116,17 @@ def create_dirs_all(list_of_data, main_folder_name,name_list):
                         shutil.copy(file_location, actual_path)
                         with open(f'{actual_path}{from_name}.tmnote', 'x', encoding='UTF-8') as file:
                             file.write(f'From_name: "{from_name}"\n{video_title}')
-                        print(actual_path + content + '___Succes_2!')
+                        msg = "[wheat1]"+actual_path + content + '___Succes_2!'
+                        statistic.log(msg)
+                        print(msg)
                     except:
                         try:
                             with open(f'{actual_path}{from_name}.tmnote', 'a', encoding='UTF-8') as file:
                                 file.write(f'''\n{video_title}''')
                         except:
-                            print(f'[red] {content} - unable to create tmnote')
+                            msg = f'[red] {content} - unable to create tmnote'
+                            statistic.err(msg)
+                            print(msg)
                             pass
                         pass
                 elif content.startswith('video'):
@@ -126,9 +143,13 @@ def create_dirs_all(list_of_data, main_folder_name,name_list):
                     try:
                         shutil.copy(file_location, actual_path + video_title)
                         create_video_note(actual_path, video_title, description, video_duration, from_name)
-                        print(actual_path + video_title + '___Succes_2!')
+                        msg = "[wheat1]"+actual_path + video_title + '___Succes_2!'
+                        statistic.log(msg)
+                        print(msg)
                     except:
-                        print(f'[red] {content} - unable to create note')
+                        msg = f'[red] {content} - unable to create note'
+                        statistic.err(msg)
+                        print(msg)
                         pass
                 elif content.startswith('https'):
                     # actual_path = path_to_save
@@ -143,7 +164,7 @@ def create_dirs_all(list_of_data, main_folder_name,name_list):
                     try:
                         create_link(actual_path, correct_url_name(content), content)
                     except:
-                        print(f'[red] {content} - unable to create link')
+                        msg = f'[red] {content} - unable to create link';statistic.err(msg);print(msg)
                         pass
                 else:
                     pass
