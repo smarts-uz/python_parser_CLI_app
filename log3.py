2d57dfec34d5edbe4bd27f08139ff460f4edd226
HunterZwer <117033000+HunterZwer@users.noreply.github.com>
2024-01-04 19:18:54 +0500

    adding utf-8 enconding to log3 and changeing log level

--- log3.py
+++ log3.py
@@ -5,17 +5,18 @@ from parsing.functions import logger_path
 class Logger():
     def __init__(self,module_name,fm):
         self.logger = logging.getLogger(module_name)
-        self.logger.setLevel(logging.INFO)
         formater = logging.Formatter("%(name)s %(asctime)s %(levelname)s %(message)s")
         self.logger.info(f"Running module {module_name}...")
-        handler2 = logging.FileHandler(f"{logger_path()}/{module_name}.log", mode=fm)
+        handler2 = logging.FileHandler(f"{logger_path()}/{module_name}.log", mode=fm, encoding='utf-8')
         handler2.setFormatter(formater)
         self.logger.addHandler(handler2)
 
     def log(self,message):
+        self.logger.setLevel(logging.DEBUG)
         self.logger.info(message)
 
-    def err(self,message):
-        self.logger.error(message)
+    def err(self, err):
+        self.logger.setLevel(logging.ERROR)
+        self.logger.exception(err)
 
 
