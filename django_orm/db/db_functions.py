bc62283fe1ab8c37718564adedde1df4f07125e5
Jamshid Masharipov <111761952+Jamshid077@users.noreply.github.com>
2024-02-17 19:42:45 +0500

    Update db_functions.py

--- django_orm/db/db_functions.py
+++ django_orm/db/db_functions.py
@@ -148,5 +148,47 @@ def get_execution_data_from_id(ex_id):
     execution = Execution.objects.get(pk=ex_id)
     return execution
 
+# ---Testing Structure Folsers function---
+# import os
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_orm.settings')
+# import django
+# django.setup()
+# from .models import *
+
+from dotenv import load_dotenv
+load_dotenv()
+root=os.getenv('PATH_TO_SAVE')
+# Testing value
+text='''Accoola | Element | Corporate | Feedback'''
+def remove_special_characters(text):
+    # O'zgartirilgan matnda faqat harflar va probellar qoladi
+    filtered_text = ''.join(char for char in text if char.isalpha() or char.isspace())
+    return filtered_text
+def correct_filename(text):
+    text = text.split('\n')
+    for line in text:
+        # line=remove_special_characters(line)
+        if '|' in line:
+            my_list = line.split(' | ')
+            my_list.reverse()
+            my_list1 = '\\'.join(my_list)
+            path = f"{root}{my_list1}"
+            return path
+        else:
+
+            line=remove_special_characters(text)
+            path = f"{root}{line}"
+            return path
+def file_creator(actual_path1):
+    actual_path=correct_filename(actual_path1)
+    if not os.path.exists(actual_path):
+        os.makedirs(actual_path)
+        print(f"Directory created sucsesfuly:{actual_path}")
+        return True
+    else:
+        print(f"Directory already created:{actual_path}")
+        return False
+# print(file_creator(actual_path1=text))
+
 
 
