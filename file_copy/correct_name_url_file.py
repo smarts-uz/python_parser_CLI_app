0d8f07f49996107c69458da7f3b3a73e7980c34a
al-dossari <jumanazarov00@bk.ru>
2024-03-01 15:00:27 +0500

    remove unsupported chars from url name

--- file_copy/correct_name_url_file.py
+++ file_copy/correct_name_url_file.py
@@ -1,38 +1,20 @@
 
 
 def correct_url_name(url):
-    if url[-1] == '/':
-        url = url[:-1] + ''
-    if 'http' or 'HTTP' in url:
-        host_name = url.split("//")
-        if 1 < len(host_name) <3:
-            host_name = host_name[1]
+    if 'https://' in url:
+        url = url.replace('https://',' ')
+    if  'http://' in url:
+        url = url.replace('http://',' ')
+    if 'www.' in url:
+        url = url.replace('www.',' ')
+    if 'search?q=' in url:
+        url = url.replace('search?q=',' ')
+    if 'exchanges?' in url:
+        url = url.replace('exchanges?',' ')
+    url = url.replace('/', ' ')
+    url = url.replace(' ',' ')
 
-        else:
-            host_name = host_name[1:]
-            host_name = '/'.join(host_name)
-        if 'search?q' in host_name:
-            name = host_name.split('search?q')[1]
-            return name
-        elif 't.me' in host_name:
-            name = host_name.replace('/',' ')
-            return name
-        else:
-            slicing = host_name.split('/')
-            if len(slicing) >=3:
-                name = slicing[-1]
-                return name
-            else:
-                if 'www' in slicing[0]:
-                    name = slicing[0].split('.')[1:]
-                    name = '.'.join(name)
-                    return name
-                else:
-                    return host_name
-    else:
-        return url
-
-
+    return url.title()
 
 
 
--- file_copy/remove_unsupported_chars/remove_http_for_folders.py
+++ file_copy/remove_unsupported_chars/remove_http_for_folders.py
@@ -1,12 +1,10 @@
 import re
 
 def correct_http_for_create_folder(text):
-    # match = re.findall('https?.*',text.lower())
     match = re.findall('[hH][tT]{2}[pP][Ss]?.*',text)
     url_name = ''.join(match).replace('//','/').split('/')[-1]
-
-    return url_name
-
+    url_name = re.sub(r'search\?q=', '', url_name)
+    return url_name.title()
 
 
 
