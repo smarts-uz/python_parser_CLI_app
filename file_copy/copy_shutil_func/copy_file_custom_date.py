c363c3ef45af90e2b530846a44b2f64abad441cd
al-dossari <jumanazarov00@bk.ru>
2024-03-19 10:57:04 +0500

    re call function

--- file_copy/copy_shutil_func/copy_file_custom_date.py
+++ file_copy/copy_shutil_func/copy_file_custom_date.py
@@ -14,21 +14,47 @@ retry_delay = int(os.getenv('retry_delay'))
 retry_tries = int(os.getenv('retry_tries'))
 retry_max_delay = int(os.getenv('retry_max_delay'))
 retry_jitter = int(os.getenv('retry_jitter'))
-@retry((FileNotFoundError, IOError), delay=retry_delay, backoff=2, max_delay=retry_max_delay, tries=retry_tries,jitter=retry_jitter)
+# @retry((FileNotFoundError, IOError), delay=retry_delay, backoff=2, max_delay=retry_max_delay, tries=retry_tries,jitter=retry_jitter)
+# def copy_file_with_custom_date(src, dst, custom_date,file_name,group_id=None):
+#     global file_dst
+#     print('Trying to copy file')
+#     # try:
+#     file_dst = shutil.copy(src=src, dst=f'{dst}/{file_name}')
+#         # file_dst = shutil.copy(src=src, dst=f'd:/pytube')
+#     if group_id !=None:
+#         update_target_group(pk=group_id, target=file_dst)
+#     # Set the custom date
+#         os.utime(file_dst, (custom_date.timestamp(), custom_date.timestamp()))
+#     return file_dst
+#     # except FileExistsError as e:
+#     #     print(e)
+#     #     send_error_msg(error=e,group_id=group_id)
+#     #     current.err(e)
+#     #     history.err(e)
+#     #     statistic.err(e)
+#
+
+
 def copy_file_with_custom_date(src, dst, custom_date,file_name,group_id=None):
     global file_dst
     print('Trying to copy file')
-    # try:
-    file_dst = shutil.copy(src=src, dst=f'{dst}/{file_name}')
-        # file_dst = shutil.copy(src=src, dst=f'd:/pytube')
-    if group_id !=None:
-        update_target_group(pk=group_id, target=file_dst)
-    # Set the custom date
-        os.utime(file_dst, (custom_date.timestamp(), custom_date.timestamp()))
-    return file_dst
-    # except FileExistsError as e:
-    #     print(e)
-    #     send_error_msg(error=e,group_id=group_id)
-    #     current.err(e)
-    #     history.err(e)
-    #     statistic.err(e)
+    try:
+        file_dst = shutil.copy(src=src, dst=f'{dst}/{file_name}')
+    # file_dst = shutil.copy(src=src, dst=f'd:/pytube')
+        if group_id != None:
+            update_target_group(pk=group_id, target=file_dst)
+        # Set the custom date
+            os.utime(file_dst, (custom_date.timestamp(), custom_date.timestamp()))
+        return file_dst
+    except FileExistsError as e:
+        retry_path = os.getenv('retry_path')
+        retry_paths  = os.path.exists(retry_path)
+        match retry_paths:
+            case True:
+                print(e)
+                send_error_msg(error=e,group_id=group_id)
+                current.err(e)
+                history.err(e)
+                statistic.err(e)
+            case False:
+                copy_file_with_custom_date(src=src, dst=dst, custom_date=custom_date,file_name=file_name,group_id=group_id)
